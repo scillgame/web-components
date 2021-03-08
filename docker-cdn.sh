5f0e4c2b83bc6f91ea5c38fe69dd2c9e636eb98b
Ivan Kresic <ivan.kresic@4players.de>
2021-03-08 15:47:00 +0000

    Delete docker-cdn.sh

--- docker-cdn.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-s3cmd put "elements/styles.css" s3://scill-gaas-01.fra1.digitaloceanspaces.com
-s3cmd put "elements/scill-widgets.js" s3://scill-gaas-01.fra1.digitaloceanspaces.com
