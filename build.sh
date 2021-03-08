e592974e7dcd9bcb1db8144b9483e9d97fccb231
Ivan Kresic <ivan.kresic@4players.de>
2021-03-08 18:37:32 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-/usr/bin/npm install && /usr/bin/npm run build:elements && /usr/bin/rm -rf package-lock.json && /usr/bin/rm -rf node_modules/ && /usr/bin/rm -rf dist/ && sudo rsync -r /home/gitlab-runner/builds/hyQTHNpm/0/scill/gaas/sdks/web-components /var/www/
+npm install && npm run build:elements && rm -rf package-lock.json && rm -rf node_modules/ && rm -rf dist/ && sudo rsync -r /home/gitlab-runner/builds/hyQTHNpm/0/scill/gaas/sdks/web-components /var/www/
