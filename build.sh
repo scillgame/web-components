8872638adb4a48923ebf2780797633245178ae7c
Ivan Kresic <ivan.kresic@4players.de>
2021-03-08 18:52:42 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-npm install && npm run build:elements && sudo rsync -r /home/gitlab-runner/builds/hyQTHNpm/0/scill/gaas/sdks/web-components /var/www/
+npm install && npm run build:elements && rsync -r /home/gitlab-runner/builds/hyQTHNpm/0/scill/gaas/sdks/web-components /var/www/
 
