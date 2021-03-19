5a0f13fd7a7d21ec5157172d6a2687b7fb009ba2
Ivan Kresic <ivan.kresic@4players.de>
2021-03-19 14:08:40 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 npm install && npm run build:elements 
 cd elements
-s3cmd put scill-widgets.js scill-widgets.css s3://scill-gaas-01/
+s3cmd put -m text/javascript scill-widgets.js -m text/css scill-widgets.css s3://scill-gaas-01/
 echo 'Setting public permissions'
 s3cmd setacl s3://scill-gaas-01/scill-widgets.js --acl-public
 s3cmd setacl s3://scill-gaas-01/scill-widgets.css --acl-public
